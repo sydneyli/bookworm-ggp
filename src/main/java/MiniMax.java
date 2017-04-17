95239a020c42ed98bc59582a77347d31d68e4113
Sydney Li <sydli@stanford.edu>
2017-04-16 19:40:00 -0700

    Added templates for assignment 2

--- src/main/java/MiniMax.java
+++ src/main/java/MiniMax.java
@@ -1,27 +1,27 @@
-import java.util.List;
+package org.ggp.base.player.gamer.statemachine.random;
 
-import org.ggp.base.apps.player.Player;
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.SimpleDetailPanel;
 import org.ggp.base.player.gamer.exception.GamePreviewException;
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
 import org.ggp.base.util.game.Game;
-import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
-import org.ggp.base.util.statemachine.Role;
 import org.ggp.base.util.statemachine.StateMachine;
-import org.ggp.base.util.statemachine.cache.CachedStateMachine;
 import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
-import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
-public class Worm extends StateMachineGamer {
+public class MiniMax extends StateMachineGamer {
 
-	Player p;
+	@Override
+	public String getName() {
+		return "Minimax";
+	}
 
 	@Override
 	public StateMachine getInitialStateMachine() {
 		// TODO Auto-generated method stub
-		return new CachedStateMachine(new ProverStateMachine());
+		return null;
 	}
 
 	@Override
@@ -35,11 +35,7 @@ public class Worm extends StateMachineGamer {
 	public Move stateMachineSelectMove(long timeout)
 			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		// TODO Auto-generated method stub
-		StateMachine machine = getStateMachine();
-		MachineState state = getCurrentState();
-		Role role = getRole();
-		List<Move> moves = machine.getLegalMoves(state, role);
-		return moves.get(0);
+		return null;
 	}
 
 	@Override
@@ -61,8 +57,7 @@ public class Worm extends StateMachineGamer {
 	}
 
 	@Override
-	public String getName() {
-		return "Worm";
+	public DetailPanel getDetailPanel() {
+		return new SimpleDetailPanel();
 	}
-
 }
