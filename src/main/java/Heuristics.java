e60c2022daf34d50e1cf2caac5be8fd12896df0d
Sydney Li <sydli@stanford.edu>
2017-04-23 17:12:58 -0700

    problem 1

--- src/main/java/Heuristics.java
+++ src/main/java/Heuristics.java
@@ -1,12 +1,21 @@
 import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
 
 /**
  * Manager for heuristics. May eventually want to record
  * state.
  */
 public class Heuristics {
-	// Dumb sample heuristic
-	public static int dumb(StateMachineGamer gamerState) {
-		return 50;
+	// Dumb sample heuristic (Problem 1)
+	public static int dumb(StateMachineGamer gamerState) throws GoalDefinitionException {
+		StateMachine stateMachine = gamerState.getStateMachine();
+		MachineState currentState = gamerState.getCurrentState();
+		if (stateMachine.isTerminal(currentState)) {
+			return 0;
+		} else {
+			return stateMachine.getGoal(currentState, gamerState.getRole());
+		}
 	}
 }
