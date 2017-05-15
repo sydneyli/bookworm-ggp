5f6724f471352ad2f93017f0d970898045b758f3
Orien Zeng <ozeng@stanford.edu>
2017-05-15 15:36:31 -0700

    Add multithreading

--- src/main/java/MCTS.java
+++ src/main/java/MCTS.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
@@ -13,6 +14,8 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 public class MCTS extends SampleGamer {
 	private volatile boolean shouldStop = false;
 	private volatile boolean debug = false;
+	private volatile MCNode root = null;
+	private static final int numThreads = 5;
 
     class Timeout extends Thread {
     	private static final int buffer = 3000;
@@ -35,6 +38,21 @@ public class MCTS extends SampleGamer {
         }
     }
 
+    class ExpandTree extends Thread {
+        ExpandTree() {
+        }
+
+        @Override
+		public void run() {
+        	try {
+				expandTree();
+			} catch (MoveDefinitionException | TransitionDefinitionException | GoalDefinitionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+    }
+
     class MCNode {
     	// If move is not null, then we only consider children in which we make that move.
     	// If move is null, there will be one child for each move we can make
@@ -44,6 +62,7 @@ public class MCTS extends SampleGamer {
     	MCNode parent = null;
     	MachineState state;
     	ArrayList<MCNode> children;
+    	Semaphore semaphore;
     	public MCNode(MachineState state, int visits, double utility, MCNode parent, ArrayList<MCNode> children, Move move) {
     		this.move = move;
     		this.state = state;
@@ -51,24 +70,29 @@ public class MCTS extends SampleGamer {
     		this.parent = parent;
     		this.utility = utility;
     		this.children = children;
+    		semaphore = new Semaphore(1);
     	}
     }
 
     double selectfn(MCNode node) {
-    	return Math.log((node.utility + 5)/node.visits) + 15 * Math.sqrt(Math.log(node.parent.visits)/node.visits);
+    	return new Random().nextDouble();//Math.log(5 + node.utility/node.visits) + 50 * Math.log(node.parent.visits)/node.visits;
     }
 
     public MCNode select (MCNode node, int depth, boolean settle) {
-    	if (node.visits == 0 && node.parent == null) {
+    	if (depth == 1 && !node.semaphore.tryAcquire()) {
+    		return null;
+    	}
+    	if ((node.visits == 0 && node.parent == null) || (settle && depth > 1 && node.move == null)) {
     		return node;
     	}
-    	if (settle && depth > 1 && node.move == null) {
-    		return node;
+    	if (node.utility < 0) {
+    		System.err.println("Utility is " +  node.utility);
+    	}
+    	if (depth > 495) {
+    		System.out.println(node.state + " " + node.move + " " + node.utility + " " + node.utility);
     	}
     	if (depth > 500) {
-    		if (new Random().nextDouble() < 0.001) {
-    			System.out.println("Depth limit");
-    		}
+    		System.out.println("Depth limit");
     		return null;
     	}
     	if (node.visits==0 && node.move == null) {
@@ -83,18 +107,25 @@ public class MCTS extends SampleGamer {
 	    		 return select(child, depth + 1, settle);
 	    	 }
 	     }
-	     double score = 0;
+	     double score = -1;
 	     MCNode result = node;
 	     if (node.children.size() == 0) {
 	    	 return null;
 	     }
 	     for (int i=0; i<node.children.size(); i++) {
 	    	 double newscore = selectfn(node.children.get(i));
+	    	  if (depth > 495) {
+	    		  System.out.println("new score " + newscore + " old score " + score + " node " + node.children.get(i).state);
+	    	  }
 	          if (newscore>score) {
 	        	  score = newscore;
 	        	  result=node.children.get(i);
 	          }
-	     };
+	     }
+
+	     if (result == node || score < 0) {
+	    	 return null;
+	     }
 	     return select(result, depth + 1, settle);
      }
 
@@ -123,14 +154,44 @@ public class MCTS extends SampleGamer {
     	}
     }
 
+    public double minMax(MCNode node, int depth) {
+    	if (depth == 0) {
+    		return node.utility;
+    	}
+    	double minVal = 101;
+    	for (MCNode child: node.children) {
+    		for (MCNode grandChild: child.children) {
+    			minVal = Math.min(minMax(grandChild, depth - 1), minVal);
+    		}
+    	}
+    	if (minVal == 101) {
+    		return node.utility;
+    	}
+    	return minVal;
+    }
+
     public boolean backpropagateH(MCNode node, double score, MCNode base, int depth) {
+    	if (node.parent == null) {
+    		return true; // No need to update parent; this would cause concurrent update
+    	}
+    	if (score < 0) {
+    		System.err.println("wtf");
+    	}
     	node.visits = node.visits+1;
-	    node.utility = node.utility+score;
 	    if (node.move != null) {
-	    	if (node.move.toString().contains("mark 1 1")) {
-	    		//System.out.print("MARK ");
+		    node.utility = node.utility+score;
+	    } else {
+	    	double min = 101;
+    		for (MCNode child: node.children) {
+    			min = Math.min(minMax(child, 1), min);
+    		}
+	    	if (min == 101) {
+	    		min = node.utility;
+	    		if (new Random().nextFloat() < 0.01) {
+	    			System.out.println("bad backprop");
+	    		}
 	    	}
-	    	//System.out.println("node " + node.state + " " + node.move + " score " + score + " base " + base.state + " " + base.move + " depth " + depth);
+	    	node.utility = min;
 	    }
 	    if (node.parent != null) {
 	    	backpropagateH(node.parent,score, node, depth + 1);
@@ -142,13 +203,7 @@ public class MCTS extends SampleGamer {
     	return backpropagateH(node, score, node, 0);
 	}
 
-	@Override
-	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-		long start = System.currentTimeMillis();
-		shouldStop = false;
-		Timeout t = new Timeout(timeout);
-		t.start();
-		MCNode root = new MCNode(getCurrentState(), 0, 0, null, new ArrayList<MCNode>(), null);
+    private void expandTree() throws MoveDefinitionException, TransitionDefinitionException, GoalDefinitionException {
 		int num = 0;
 		int numBad = 0;
 		int numContinues = 0;
@@ -170,12 +225,60 @@ public class MCTS extends SampleGamer {
 			}
 			//System.out.println("Score " + score + " state " + terminal);
 			backpropagate(s, score);
+			releaseLocks(s);
 			num++;
-			if (num % 50 == 0) {
+			if (num % 5000 == 0) {
 				System.out.println("Performed " + num + " depth charges " + numBad + " errors, got score " + score);
 			}
 		}
 		System.out.println("Stopped");
+    }
+
+	private void releaseLocks(MCNode s) {
+		// TODO Auto-generated method stub
+		if (s == null) {
+			return;
+		}
+		if (s.parent != null && s.parent.parent == null) {
+			if (s.semaphore.availablePermits() != 0) {
+				System.out.println("Bad permit");
+			} else {
+				s.semaphore.release();
+			}
+		}
+		releaseLocks(s.parent);
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		long start = System.currentTimeMillis();
+		shouldStop = false;
+		Timeout t = new Timeout(timeout);
+		t.start();
+		root = new MCNode(getCurrentState(), 0, 0, null, new ArrayList<MCNode>(), null);
+		expand(root);
+		root.visits++;
+
+		for (int i = 0; i < numThreads; i++) {
+			ExpandTree et = new ExpandTree();
+			et.start();
+			if (i == 0) {
+				try { // First thread gets head start
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		expandTree();
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		double bestScore = -1;
 		Move bestMove = null;
@@ -195,6 +298,10 @@ public class MCTS extends SampleGamer {
 
 		long stop = System.currentTimeMillis();
 		t.stop();
+		if (selection == null) {
+			System.out.println("null move!");
+			selection = moves.get(0);
+		}
 		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
 		return selection;
 	}
